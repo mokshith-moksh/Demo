ebbba18b6219c6924ade8b1932b59a7651f99844
mokshith-moksh <mokshithmokshith7@gmail.com>
2025-03-23 09:32:49 +0530

    progess-6

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <div className="bg-transparent w-[100vw] min-h-screen flex flex-col gap-12 lg:flex-row justify-center items-center lg:gap-28 p-4">
       {/* Tabs Section */}
-      <div className="w-1/2 lg:w-auto mt-52 lg:mt-0 relative">
+      <div className="w-full lg:w-auto mt-52 lg:mt-0 relative">
         <Tabs defaultValue="account" className="w-full relative">
           <TabsList className="w-auto">
             <TabsTrigger value="account" className="w-auto">
